47d6b63b060b2108cdc1b05150cd1d3a22c38a61
aleksander.gutowski <aleksander.gutowski@marconicloud.it>
2025-05-19 09:35:11 +0000

    add dynamic posts

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, send_file, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="src")
+
+posts = [
+    {'title':"Il Futuro dell'AI nel 2024", 'content':"Un'analisi sulle tendenze e le previsioni per l'intelligenza artificiale nell'anno in corso."},
+    {'title':"Come iniziare con il Machine Learning", 'content':"Una guida per principianti sui primi passi nel mondo del Machine Learning."},
+    {'title':"Discussione: AI Etica e Responsabile", 'content':"Condividi i tuoi Medication sull'importanza di uno sviluppo etico e responsabile dell'AI."}
+]
 
 @app.route("/")
 def index():
-    return send_file('src/index.html')
+    return render_template('index.html', posts=posts)
 
 def main():
     app.run(port=int(os.environ.get('PORT', 80)))
